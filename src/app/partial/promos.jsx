7dcbf99689e36a629eef9833f7970812f6a4db7e
farisqlail <faris.riskilail@gmail.com>
2025-01-27 21:59:50 +0700

    add: promo home

--- src/app/partial/promos.jsx
+++ src/app/partial/promos.jsx
@@ -41,88 +41,40 @@ export function Promos() {
     return (
         <>
             {promos.length > 0 && (
-                <section className="px-8 py-8">
-                    <div className="container mx-auto text-center">
-                        <Typography variant="h2" color="white" className="mb-4">
-                            Promo Menarik
-                        </Typography>
+                <section className="mt-6">
+                    <div className="flex gap-4 pl-8 items-center pr-8">
+                        <h1 className="mb-4 text-[#ba0c0c] lg:text-3xl text-2xl lg:text-center text-left text-nowrap font-semibold">
+                            Produk Hari Ini
+                        </h1>
+                        <div className="w-full bg-[#ba0c0c] p-[1px]"></div>
+                    </div>
 
-                        <div className="lg:p-8 p-0">
-                            {isLoading ? (
-                                <Swiper
-                                    modules={[Navigation, Pagination]}
-                                    spaceBetween={30}
-                                    navigation
-                                    pagination={{ clickable: true }}
-                                    className="mySwiper"
-                                    style={{ maxHeight: "auto" }}
-                                    breakpoints={{
-                                        640: {
-                                            slidesPerView: 1,
-                                        },
-                                        768: {
-                                            slidesPerView: 2,
-                                        },
-                                        1024: {
-                                            slidesPerView: 3,
-                                        },
-                                        1280: {
-                                            slidesPerView: 4,
-                                        },
-                                    }}
-                                >
-                                    {Array(8)
-                                        .fill(0)
-                                        .map((_, index) => (
-                                            <SwiperSlide key={index} className="relative rounded-lg overflow-hidden">
-                                                <div className="h-[250px] w-full bg-gray-200 animate-pulse rounded-lg"></div>
-                                                <div className="p-4">
-                                                    <div className="h-6 bg-gray-300 animate-pulse rounded w-3/4 mb-2"></div>
-                                                    <div className="h-4 bg-gray-300 animate-pulse rounded w-1/2"></div>
-                                                </div>
-                                            </SwiperSlide>
-                                        ))}
-                                </Swiper>
-                            ) : (
-                                <Swiper
-                                    modules={[Navigation]}
-                                    spaceBetween={30}
-                                    navigation
-                                    className="mySwiper"
-                                    style={{ maxHeight: "auto" }}
-                                    breakpoints={{
-                                        640: {
-                                            slidesPerView: 2,
-                                        },
-                                        768: {
-                                            slidesPerView: 2,
-                                        },
-                                        1024: {
-                                            slidesPerView: 3,
-                                        },
-                                        1280: {
-                                            slidesPerView: 4,
-                                        },
-                                    }}
-                                >
-                                    {promos.map((promo) => (
-                                        <SwiperSlide key={promo.id} className="relative rounded-lg overflow-hidden">
-                                            <Image
-                                                src={`${promo.image}`}
-                                                alt={promo.title}
-                                                width={300}
-                                                height={250}
-                                                className="object-cover h-[250px] w-full"
-                                            />
-                                            <div className="pt-4 text-left">
-                                                <Typography variant="h5" className="font-semibold text-white">{promo.title}</Typography>
-                                                <Typography variant="body2" className="text-gray-500">{promo.deskripsi}</Typography>
-                                            </div>
-                                        </SwiperSlide>
-                                    ))}
-                                </Swiper>
-                            )}
-                        </div>
+                    <div className="flex gap-6 lg:pl-8 lg:pr-8 lg:pt-16 lg:pb-16 pl-8 pt-8 pr-8 pb-8 w-full overflow-x-auto hidden-scrollbar">
+                        {isLoading ? Array(8)
+                            .fill(0)
+                            .map((_, index) => (
+                                <div key={index} className="h-full flex items-center w-full">
+                                    <div className="h-[250px] w-[400px] bg-gray-200 animate-pulse rounded-lg"></div>
+                                    <div className="p-4">
+                                        <div className="h-6 bg-gray-300 animate-pulse rounded w-3/4 mb-2"></div>
+                                        <div className="h-4 bg-gray-300 animate-pulse rounded w-1/2"></div>
+                                    </div>
+                                </div>
+                            )) : promos.map((promo) => (
+                                <div key={promo.id} className="carousel-item-product">
+                                    <Image
+                                        src={`${promo.image}`}
+                                        alt={promo.title}
+                                        width={300}
+                                        height={250}
+                                        className="object-cover h-[250px] w-full"
+                                    />
+                                    <div className="pt-4 text-left">
+                                        <Typography variant="h5" className="font-semibold text-white">{promo.title}</Typography>
+                                        <Typography variant="body2" className="text-gray-500">{promo.deskripsi}</Typography>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
                 </section>
             )}
