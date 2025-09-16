24e40c7b449895e439f75a7cb892d218e807b2cf
farisqlail <faris.riskilail@gmail.com>
2025-09-16 19:46:38 +0700

    fix

--- src/app/success/partial/SuccessPaymentCard.jsx
+++ src/app/success/partial/SuccessPaymentCard.jsx
@@ -133,6 +133,11 @@ const SuccessPaymentCard = ({ dataAccount }) => {
         setShowModal(false);
     }
 
+    const backToHome = () => {
+        localStorage.removeItem("dataPayment");
+        router.push("/");
+    }
+
     return (
         <div className="flex items-center justify-center min-h-screen bg-inherit p-4">
             <div className="bg-white p-4 rounded-lg shadow-md text-center w-fit">
@@ -161,7 +166,7 @@ const SuccessPaymentCard = ({ dataAccount }) => {
                     <div className="flex justify-center">
                         <button
                             className="px-4 py-2 bg-[#ba0c0c] text-white rounded-md"
-                            onClick={toHome}
+                            onClick={backToHome}
                         >
                             Kembali ke Beranda
                         </button>
